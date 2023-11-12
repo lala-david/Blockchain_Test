6a7dbd8a47ef1e7d7de894439a9a1ce286721171
lala-david <wnswns1946@naver.com>
2023-11-12 19:47:50 +0900

    #2 ♾ ci/cd

---
+++ test/test_code.ts
@@ -0,0 +1,24 @@
+import { expect } from "chai";
+import { createBlockchain, addBlock } from "../Node/src/Blockchain";
+import { createProofOfWork, validate } from "../Node/src/ProofOfWork";
+
+describe('Blockchain Test', () => {
+  it('should create new blockchain', () => {
+    const blockchain = createBlockchain();
+    expect(blockchain.blocks).to.have.lengthOf(1);  
+  });
+
+  it('should add new block to blockchain', () => {
+    const blockchain = createBlockchain();
+    addBlock(blockchain, "New Block");
+    expect(blockchain.blocks).to.have.lengthOf(2);
+  });
+
+  it('should validate Proof of Work', () => {
+    const blockchain = createBlockchain();
+    addBlock(blockchain, "New Block");
+    const block = blockchain.blocks[1];
+    const pow = createProofOfWork(block);
+    expect(validate(pow)).to.equal(true);
+  });
+});
